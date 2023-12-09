5ad114bdd56b4b9dedacbb7bb66bdcc7b34bede0
DakshSahani <dakshsahani8123@gmail.com>
2023-12-09 11:32:25 +0530

    error-handler removing import-export

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,9 +1,9 @@
-import { StatusCodes } from "http-status-codes";
-import CustomAPIError from "./custom-api.js";
+const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./custom-api.js");
 
 class NOT_FOUND extends CustomAPIError{
     constructor(message){
         super(messageStatusCodes.NOT_FOUND);
     }
 };
-export default  NOT_FOUND;
+module.exports = NOT_FOUND;
