5f5661f294202c986a9be759b6c3c2e5b7f8a847
DakshSahani <dakshsahani8123@gmail.com>
2023-12-16 01:18:34 +0530

    course Routes

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const courseController = require('../controllers/courseController')
 
-
-router.route("/get-all-courses") // wiht search functionality 
-router.route("/add-course")
-router.route("/delete-course")
-router.route("update-course") // ??? 
+router.get("/",courseController.getAllCourses) // wiht search functionality 
+router.get("/:commonId", courseController.getCourse)
+router.get("/basic-info/:commonId", courseController.getBasicInfo)
+router.get("/categories/:commonId", courseController.getCategory)
+router.get("/semesters/:commonId", courseController.getSemester)
+router.get("/subjects/:commonId", courseController.getSubjects)
+// router.route("/course").get()
 router.route("/get-developers") // including admins
 
 module.exports = router;
