d2596dadcb9aaaa5ceb7c69f165893f6aadbd10e
Akshat Tripathi <114927070+Akshat-1403@users.noreply.github.com>
2023-12-17 17:02:44 +0530

    Update resourcesRoute.js

--- routes/resourcesRoute.js
+++ routes/resourcesRoute.js
@@ -4,6 +4,5 @@ const router = express.Router();
 
 router.route("/get-resouces").get(getAllResources) // with search functionality 
 router.route("/add-resource").post(addResources)
-router.route("/delete-resouce").delete(deleteResources)
 
 module.exports = router;
