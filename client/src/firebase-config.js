5c37604ee8481865aa97da843bb0f28bef545772
Akshat Tripathi <114927070+Akshat-1403@users.noreply.github.com>
2023-12-19 16:48:12 +0530

    Update firebase-config.js

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 
@@ -16,5 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+// export const auth = getAuth(app);
+// export const provider = new GoogleAuthProvider();
