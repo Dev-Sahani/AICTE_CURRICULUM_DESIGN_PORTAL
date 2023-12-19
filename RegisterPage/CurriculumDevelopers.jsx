b163f5ebc91bfa501f6a931b657cd85fbbdf5370
ritik sharma <138618220+ritik240sharma@users.noreply.github.com>
2023-12-19 17:43:08 +0530

    added register

---
+++ RegisterPage/CurriculumDevelopers.jsx
@@ -0,0 +1,17 @@
+function CurriculumDevelopers(){
+    return(
+    <>
+       
+       <div className="flex  bg-white justify-center content-center  rounded-lg ">
+          
+          <form className="flex  ">
+            <label className="text-blue-200">UserId</label>
+            <input type="text" placeholder="Enter user Id" className="w-fit"/>
+          </form>
+
+       </div>
+
+    </>
+    )
+}
+export default CurriculumDevelopers
