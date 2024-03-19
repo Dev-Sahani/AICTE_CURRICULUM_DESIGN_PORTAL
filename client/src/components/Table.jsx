8d39eb7e310f3de0c5cbd4870f2e097eaecf8e82
Dev-Sahani <devdaksh8102003@gmail.com>
2024-03-19 16:58:18 +0530

    some ui changes in add subject

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,34 +1,44 @@
 import { Link } from "react-router-dom";
 
 export default function Table({data, keys, primaryHeader=true, secondaryHeader=false, to}) {
-  if(!data || !keys) return <div>Data is not passed</div>;
+  if(!data || !keys || !Array.isArray(keys)) return <div>Data is not passed</div>;
 
   const rowClasses = "w-full py-2 px-2 lg:px-6 xl:px-10 text-center bg-accent-400";
 
   return (
     <div className="w-full flex justify-center items-center">
       {
-        keys?.map((value, indx)=>{
+        [...keys, "-" ].map((value, indx)=>{
           return (
-            <div className="w-full flex flex-col items-center justify-between whitespace-nowrap" key={indx}>
-              <header className={`${secondaryHeader ? "w-full text-center text-white bg-primary-700 p-2" : "text-primary-500"} text-lg font-semibold capitalize`}>
-                <h2>{value}</h2>
+            <div className={`w-full flex flex-col ${value==="-" ? "items-start" : "items-center"} justify-between whitespace-nowrap`} key={indx}>
+              <header className={`${secondaryHeader ? `text-center text-white bg-primary-700 p-2` : "text-primary-500"} ${value==="-" ? "w-fit px-5" : "w-full"} text-lg font-semibold capitalize`}>
+                <h2 className={`${value==="-" && "text-transparent"} text-center`}>{value}</h2>
               </header>
               
-              <main className="w-full flex flex-col items-center gap-2 mt-2">
+              <main className={`w-full flex flex-col ${value==="-" ? "items-start" : "items-center"} gap-2 mt-2`}>
                 {
                   data?.map((item, ind)=>{
                     const classes = `
                       ${rowClasses} 
                       ${indx===0 && " rounded-l-full "} 
-                      ${indx===keys.length-1 && " rounded-r-full"}
+                      ${value === "-" && "rounded-r-full !w-fit !px-3"}
                     `
+
                     let innerContent = "-"
                     if(item && item.cur && item.cur[value]) 
                       innerContent = item.cur[value];
                     
-                    // p can be replaced by input to listen any changes in subject property
-                    return <Link key={ind} to={`${item.cur.common_id}`} className={classes}>{ innerContent }</Link>
+                    return (
+                      value === "-" 
+                      ?
+                      <button className={classes} key={ind} onClick={()=>{}}>
+                        <img src="/deleteButton.png" className="h-6 min-w-6" alt="delete" /> 
+                      </button>
+                      : 
+                      <Link key={ind} to={`${item.cur.common_id}`} className={classes}>
+                        { innerContent }
+                      </Link> 
+                    )
                   })
                 }
               </main>
